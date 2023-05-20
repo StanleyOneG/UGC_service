732500d2566778b815c752d2657405aea6784839
Aleksandr Buliga <buliga.alexander@yandex.ru>
2023-05-20 21:18:31 +0400

    change id type to String

--- etl-kafka-ch/init-db/engine.sh
+++ etl-kafka-ch/init-db/engine.sh
@@ -1,7 +1,7 @@
 set -e
 clickhouse client -n <<-EOSQL
   CREATE TABLE IF NOT EXISTS progress_queue (
-    id Int32,
+    id String,
     user_movie_id String,
     timestamp UInt64)
     ENGINE = Kafka
