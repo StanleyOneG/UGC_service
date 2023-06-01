55a291f423545082ce1bdab3e54da25b690059a1
Stanislav <goncharuk.sm@yandex.ru>
2023-06-01 16:01:54 +0000

    Fix Makefile, fix mongo configuration script

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 service_up:
-	docker-compose -f kafka/docker-compose.yaml up --build -d
-	docker-compose -f etl-kafka-ch/docker-compose.yaml up --build -d
-	docker-compose -f mongodb/docker-compose.yaml up --build -d
+	docker compose -f kafka/docker-compose.yaml up --build -d
+	docker compose -f etl-kafka-ch/docker-compose.yaml up --build -d
+	docker compose -f mongodb/docker-compose.yaml up --build -d
 	./mongodb/configuration.sh
-	docker-comopse -f ugc_api/docker-compose.yaml up --build -d
+	docker compose -f ugc_api/docker-compose.yaml up --build -d
--- mongodb/configuration.sh
+++ mongodb/configuration.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-while [ "`sudo docker inspect -f {{.State.Health.Status}} mongors1n1`" != "healthy" ]; do sleep 2; done
+while [ "`docker inspect -f {{.State.Health.Status}} mongors1n1`" != "healthy" ]; do sleep 2; done
 
 echo "MONGODB CLUSTER IS READY"
 
-sudo docker exec -it mongocfg1 bash -c 'echo "rs.initiate({_id: \"mongors1conf\", configsvr: true, members: [{_id: 0, host: \"mongocfg1\"}, {_id: 1, host: \"mongocfg2\"}, {_id: 2, host: \"mongocfg3\"}]})" | mongosh'
+docker exec -it mongocfg1 bash -c 'echo "rs.initiate({_id: \"mongors1conf\", configsvr: true, members: [{_id: 0, host: \"mongocfg1\"}, {_id: 1, host: \"mongocfg2\"}, {_id: 2, host: \"mongocfg3\"}]})" | mongosh'
 
-sudo docker exec -it mongors1n1 bash -c 'echo "rs.initiate({_id: \"mongors1\", members: [{_id: 0, host: \"mongors1n1\"}, {_id: 1, host: \"mongors1n2\"}, {_id: 2, host: \"mongors1n3\"}]})" | mongosh'
+docker exec -it mongors1n1 bash -c 'echo "rs.initiate({_id: \"mongors1\", members: [{_id: 0, host: \"mongors1n1\"}, {_id: 1, host: \"mongors1n2\"}, {_id: 2, host: \"mongors1n3\"}]})" | mongosh'
 
-sudo docker exec -it mongos1 bash -c 'echo "sh.addShard(\"mongors1/mongors1n1\")" | mongosh'
+docker exec -it mongos1 bash -c 'echo "sh.addShard(\"mongors1/mongors1n1\")" | mongosh'
 
-sudo docker exec -it mongors2n1 bash -c 'echo "rs.initiate({_id: \"mongors2\", members: [{_id: 0, host: \"mongors2n1\"}, {_id: 1, host: \"mongors2n2\"}, {_id: 2, host: \"mongors2n3\"}]})" | mongosh'
+docker exec -it mongors2n1 bash -c 'echo "rs.initiate({_id: \"mongors2\", members: [{_id: 0, host: \"mongors2n1\"}, {_id: 1, host: \"mongors2n2\"}, {_id: 2, host: \"mongors2n3\"}]})" | mongosh'
 
-sudo docker exec -it mongos1 bash -c 'echo "sh.addShard(\"mongors2/mongors2n1\")" | mongosh'
+docker exec -it mongos1 bash -c 'echo "sh.addShard(\"mongors2/mongors2n1\")" | mongosh'
